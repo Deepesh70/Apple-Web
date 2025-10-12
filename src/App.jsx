d2e2aedacc246a6a9096879fee34be1db15d31b5
Deepesh70 <deepeshdangi700@gmail.com>
2025-10-12 10:01:07 +0530

    zustand + canvas store

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import Productviewer from './components/Productviewer';
 
 const App = () => {
   return (
     <main>
       <Navbar />
       <Hero />
+      <Productviewer />
     </main>
   )
 }
