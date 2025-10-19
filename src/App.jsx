369c5e743f4054964b08b1467a6e96a70b669137
Deepesh70 <deepeshdangi700@gmail.com>
2025-10-19 14:32:10 +0530

    showcase section with video

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Productviewer from './components/Productviewer';
+import Showcase from './components/Showcase';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/all';
 
@@ -13,6 +14,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <Productviewer />
+      <Showcase />
     </main>
   )
 }
