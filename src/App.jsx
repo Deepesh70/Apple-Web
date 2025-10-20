7999fdea986d90ac13517b3d76ae2d425b8e9614
Deepesh70 <deepeshdangi700@gmail.com>
2025-10-20 18:35:49 +0530

    setup all the remaining sections

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import Hero from './components/Hero';
 import Productviewer from './components/Productviewer';
 import Showcase from './components/Showcase';
 import gsap from 'gsap';
+import Performance from './components/Performance';
+import Features from './components/Features';
+import Highlights from './components/Highlights';
+import Footer from './components/footer';
 import { ScrollTrigger } from 'gsap/all';
 
 gsap.registerPlugin(ScrollTrigger);
@@ -15,6 +19,10 @@ const App = () => {
       <Hero />
       <Productviewer />
       <Showcase />
+      <Performance />
+      <Features />
+      <Highlights />
+      <Footer />
     </main>
   )
 }
